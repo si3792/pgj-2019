d0541e8bffb6b33fe580621cfaafd4003cf21058
DESKTOP-PEJCES4\DeathToAllPigeons <pavkat97@gmail.com>
2019-01-26 02:13:18 +0200

    Added player movent and attack.

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        float attackTime = GetComponentInParent<Character>().attackTime;
+        Invoke("ExecuteSelf", attackTime);
+    }
+
+    private void ExecuteSelf() {
+        GetComponentInParent<Character>().attacking = false;
+        gameObject.SetActive(false);
+    }
+}
