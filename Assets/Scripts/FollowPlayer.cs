9d39d96345e2f1898ac2d2dd8e6ed1b69578c4aa
zgurovski <zgurovski00@gmail.com>
2019-01-26 23:43:40 +0200

    background + follow player

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -6,7 +6,8 @@ public class FollowPlayer : MonoBehaviour {
     public float interpVelocity;
     public float minDistance;
     public float followDistance;
-    public float clipLeft, clipRight;
+	public float clipLeft, clipRight;
+	public float clipUp, clipDown;
     private GameObject target;
     public Vector3 offset;
     Vector3 targetPos;
@@ -24,19 +25,19 @@ public class FollowPlayer : MonoBehaviour {
         {
             Vector3 posNoZ = transform.position;
             posNoZ.z = target.transform.position.z;
-            posNoZ.y = target.transform.position.y;
 
             Vector3 targetDirection = (target.transform.position - posNoZ);
 
             interpVelocity = targetDirection.magnitude * 5f;
 
             targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
-            targetPos = new Vector3(targetPos.x, 0, targetPos.z);
+            targetPos = new Vector3(targetPos.x, targetPos.y, targetPos.z);
             transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
 
-            // Apply left and right limits
-            var clampedX = Mathf.Clamp(transform.position.x, clipLeft, clipRight);
-            transform.position = new Vector3 (clampedX, transform.position.y, transform.position.z);
+			// Apply left and right limits
+			var clampedX = Mathf.Clamp(transform.position.x, clipLeft, clipRight);
+			var clampedY = Mathf.Clamp(transform.position.y, clipDown, clipUp);
+			transform.position = new Vector3 (clampedX, clampedY, transform.position.z);
         }
     }
 
