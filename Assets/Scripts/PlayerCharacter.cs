cc4c2446149191e9f48acccc1e522330e50643aa
DESKTOP-PEJCES4\DeathToAllPigeons <pavkat97@gmail.com>
2019-01-26 09:32:08 +0200

    Added player missile.

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class PlayerCharacter : Character
 {
+    public GameObject shockWave;
+    public int shockWaveCost = 1;
+    public float shockWaveCoolDown = 5.0f;
+    public bool canUseShockWave = true;
+    public GameObject playeMissile;
+    public int missileCost = 1;
+    public float missileCoolDown = 5.0f;
+    public bool canUseMissile = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +26,10 @@ public class PlayerCharacter : Character
             Attack();
         }
         if (Input.GetButtonDown("Fire2")) {
-            Attack();
+            SpawnShockWave();
+        }
+        if (Input.GetButtonDown("Fire3")) {
+            SpawnMissile();
         }
     }
 
@@ -33,5 +45,46 @@ public class PlayerCharacter : Character
         currentHitPoints += healthValue;
         if (currentHitPoints > maxHitPoints) { maxHitPoints += currentHitPoints - maxHitPoints; }
         Debug.Log(power + " " + currentHitPoints + " " + maxHitPoints);
+    }
+
+    private void SpawnShockWave() {
+        if (currentHitPoints <= shockWaveCost || !canUseShockWave || !canAttack) return;
+        shockWave.gameObject.SetActive(true);
+        currentHitPoints -= shockWaveCost;
+        attacking = true;
+        canAttack = false;
+        canUseShockWave = false;
+        Invoke("ResetAttackingFlag", attackTime);
+        Invoke("ResetAttack", attackCoolDown);
+        Invoke("ReserShockWave", shockWaveCoolDown);
+    }
+
+    private void ReserShockWave() {
+        canUseShockWave = true;
+        Debug.Log("Can use shock wave " + currentHitPoints);
+    }
+
+    private void SpawnMissile() {
+        if (currentHitPoints <= missileCost || !canUseMissile || !canAttack) return;
+        Debug.Log(canUseMissile);
+        Vector3 position = transform.position;
+        position.x += facingRight ? 1 : -1;
+        GameObject newMissile =  (GameObject)Instantiate(playeMissile, position, Quaternion.identity);
+        newMissile.SetActive(true);
+        newMissile.GetComponent<PlayerMissile>().SetTarget(position, facingRight, power);
+        currentHitPoints -= missileCost;
+        attacking = true;
+        canAttack = false;
+        canUseMissile = false;
+        Invoke("ResetAttackingFlag", attackTime);
+        Invoke("ResetAttack", attackCoolDown);
+        Invoke("ResetMissile", shockWaveCoolDown);
+
+    }
+
+
+    private void ResetMissile() {
+        canUseMissile = true;
+        Debug.Log("Can use missile " + currentHitPoints);
     }
 }
---
+++ Assets/Scripts/PlayerMissile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMissile : MonoBehaviour
+{
+    public int damage = 1;
+    public float powerMultiplire;
+    public bool goingRight = false;
+    public float acceleration;
+    public float range;
+    private Vector3 targetPosition;
+
+    /*
+    public void OnEnable() {
+        Debug.Log("spawing");
+        Character player = GameObject.Find("Player").GetComponent<Character>();
+        damage = Mathf.RoundToInt(player.power * powerMultiplire);
+        goingRight = player.facingRight;
+        targetPosition = transform.position;
+        targetPosition.x += goingRight ? range : -range;
+        Debug.Log(targetPosition);
+    }
+    */
+    public void SetTarget(Vector3 currentPosition, bool facingRight, int power) {
+        targetPosition = currentPosition;
+        damage = Mathf.RoundToInt(power * powerMultiplire);
+        goingRight = facingRight;
+        targetPosition.x += goingRight ? range : -range;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, acceleration);
+        if(transform.position == targetPosition) { Destroy(this.gameObject); }
+    }
+}
